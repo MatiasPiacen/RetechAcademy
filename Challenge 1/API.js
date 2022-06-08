cb262929c6819b8218d6cf0362a217b4f26633cd
MatiasPiacen <matiaspiacen@gmail.com>
2022-06-07 21:14:58 -0300

    cambios

--- Challenge 1/API.js
+++ Challenge 1/API.js
@@ -1,47 +1,48 @@
-///////////////////////////////////API Funcionando///////////////////////////////////////////////
 let pagina = 1;
-const cargarPelis = async() => {
-    try{ 
-    const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=93df802a9e529fe0cfd6000a6d47254c&language=es-MX&page=${pagina}`);
+function dibujarTarjetasDePeliculas(peliculas) {
+ let misPeliculas = "";
+ peliculas.forEach((pelicula) => {
+    misPeliculas += `
+              <div class="pelicula">
+                  <img  class="img-pel" src="https://image.tmdb.org/t/p/w500/${peliculas.poster_path}" >
+                  <div id="info">
+                      <h2 id="titulo-pel">${pelicula.title}</h2>
+                      <h3 id="genero">${pelicula.genres_movies}</h3>
+                      <h4 id="año">${pelicula.release_date}</h4>
+                  </div>
+                  <div id="descripcion">
+                      <h1>${pelicula.title}</h1>
+                      <p>${pelicula.overview}</p>
+                      </div>
+              </div>
+          `;
+});
 
-     console.log(respuesta);
+document.getElementById("main").innerHTML = misPeliculas;
+}
 
-     if(respuesta.status ===200){ 
-     const datos = await respuesta.json();
-     let Mispeliculas = ''
-     datos.results.forEach(peliculas => {
-        Mispeliculas +=  `
-            <div class="pelicula">
-                <img  class="img-pel" src="https://image.tmdb.org/t/p/w500/${peliculas.poster_path}" >
-                <div id="info">
-                    <h2 id="titulo-pel">${peliculas.title}</h2>
-                    <h3 id="genero">${peliculas.genres_movies}</h3>
-                    <h4 id="año">${peliculas.release_date}</h4>
-                </div>
-                <div id="descripcion">
-                    <h1>${peliculas.title}</h1>
-                    <p>${peliculas.overview}</p>
-                    </div>
-            </div>
-        `
-     });
-     
-     document.getElementById('main').innerHTML = Mispeliculas; 
+const cargarPelis = async () => {
+  try {
+    const respuesta = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=93df802a9e529fe0cfd6000a6d47254c&language=es-MX&page=${pagina}`
+    );
+
+    console.log(respuesta);
+
+    if (respuesta.status === 200) {
+      const datos = await respuesta.json();
+      dibujarTarjetasDePeliculas(datos.result)
     }
     //////////////Error por si no pongo el condicional del boton anterior////////////////////////////
-    else if(respuesta.status ===401){
-         console.log('Algo anda mal')
-     }
-     else if(respuesta.status ===404){
-         console.log('La pelicula que buscas no existe')
-     }
-     
+    else if (respuesta.status === 401) {
+      console.log("Algo anda mal");
+    } else if (respuesta.status === 404) {
+      console.log("La pelicula que buscas no existe");
     }
-    catch(error){
-         console.log(error);
-     }
-    }
-cargarPelis();
+  } catch (error) {
+    console.log(error);
+  }
+};
 /////////////////////////////////Botones/////////////////////////////////////////////////////////////
 const btnAnterior = document.getElementById('btnAnterior');
 const btnSiguiente = document.getElementById('btnSiguiente');
@@ -60,23 +61,23 @@ btnAnterior.addEventListener('click', () => {
 	}
 });
 ///////////////////////////////////////BUSCADOR////////////////////////////////////////////////////
-function buscarPeliculas () {
-      const nombreDePelicula = document.getElementById("nombre-peliculas").value;
-      fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=af0e0a76ec3a39a7dc32e7f88e6e6968&language=es-MX&query=${nombreDePelicula}&page=1&,`,
-        {
-          method: "GET",
-          redirect: "follow",
-        }
-      )
-        .then((success) => success.json())
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
- }
+function buscarPeliculas() {
+  const nombreDePelicula = document.getElementById("nombre-peliculas").value;
+  fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=af0e0a76ec3a39a7dc32e7f88e6e6968&language=es-MX&query=${nombreDePelicula}&page=1&,`,
+    {
+      method: "GET",
+      redirect: "follow",
+    }
+  )
+    .then((success) => success.json())
+    .then((datos) => {
+       dibujarTarjetasDePeliculas(datos.result)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+} 
  let BotonSearch = document.querySelector("#boton");
  let Botonclear = document.querySelector("#clear")
  let inputPeliculas = document.querySelector("#nombre-peliculas");
---
+++ Challenge 1/main.js
@@ -0,0 +1,98 @@
+///////////////////////////////////API Funcionando///////////////////////////////////////////////
+let pagina = 1;
+const cargarPelis = async() => {
+    try{ 
+    const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=93df802a9e529fe0cfd6000a6d47254c&language=es-MX&page=${pagina}`);
+
+     console.log(respuesta);
+
+     if(respuesta.status ===200){ 
+     const datos = await respuesta.json();
+     let Mispeliculas = ''
+     datos.results.forEach(peliculas => {
+        Mispeliculas +=  `
+            <div class="pelicula">
+                <img  class="img-pel" src="https://image.tmdb.org/t/p/w500/${peliculas.poster_path}" >
+                <div id="info">
+                    <h2 id="titulo-pel">${peliculas.title}</h2>
+                    <h3 id="genero">${peliculas.genres_movies}</h3>
+                    <h4 id="año">${peliculas.release_date}</h4>
+                </div>
+                <div id="descripcion">
+                    <h1>${peliculas.title}</h1>
+                    <p>${peliculas.overview}</p>
+                    </div>
+            </div>
+        `
+     });
+     
+     document.getElementById('main').innerHTML = Mispeliculas; 
+    }
+    //////////////Error por si no pongo el condicional del boton anterior////////////////////////////
+    else if(respuesta.status ===401){
+         console.log('Algo anda mal')
+     }
+     else if(respuesta.status ===404){
+         console.log('La pelicula que buscas no existe')
+     }
+     
+    }
+    catch(error){
+         console.log(error);
+     }
+    }
+cargarPelis();
+/////////////////////////////////Botones/////////////////////////////////////////////////////////////
+const btnAnterior = document.getElementById('btnAnterior');
+const btnSiguiente = document.getElementById('btnSiguiente');
+
+btnSiguiente.addEventListener('click', () => {
+	if(pagina < 1000){
+		pagina += 1;
+		cargarPelis();
+	}
+});
+
+btnAnterior.addEventListener('click', () => {
+	if(pagina > 1){
+		pagina -= 1;
+		cargarPelis();
+	}
+});
+///////////////////////////////////////BUSCADOR////////////////////////////////////////////////////
+function buscarPeliculas () {
+      const nombreDePelicula = document.getElementById("nombre-peliculas").value;
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=af0e0a76ec3a39a7dc32e7f88e6e6968&language=es-MX&query=${nombreDePelicula}&page=1&,`,
+        {
+          method: "GET",
+          redirect: "follow",
+        }
+      )
+        .then((success) => success.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+ }
+ let BotonSearch = document.querySelector("#boton");
+ let Botonclear = document.querySelector("#clear")
+ let inputPeliculas = document.querySelector("#nombre-peliculas");
+ let url;
+
+ BotonSearch.addEventListener("click",() =>{
+   defaultAll(main)
+   var nombre_pelicula = inputPeliculas.value;
+   url = `https://api.themoviedb.org/3/search/movie?api_key=af0e0a76ec3a39a7dc32e7f88e6e6968&language=en-US&query=${nombre_pelicula}&page=1&`;
+   buscarPeliculas(url, main)
+ })
+ Botonclear.addEventListener("click", () => { 
+    defaultAll(main) 
+    inputPeliculas.value = ""; 
+    popular() 
+})
+ function defaultAll(main) {
+    main.innerHTML = ""; }
+ 
